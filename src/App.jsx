5b728e36888da39c44f94d3816244b07a20a7b39
BogdanWazer <ki4ack.bogdam@gmail.com>
2023-07-07 21:18:59 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+function User({ name, lastName, ...rest }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{lastName}</h2>
+      <p>{JSON.stringify(rest)}</p>
+    </div>
+  );
+}
 function App() {
   const variable = 'Alex';
 
   const fruites = ['banana', 'apple', 'orange'];
+
+  const [banana, ...restFruits] = fruites;
+
+  console.log(fruites);
 
   const fruites2 = fruites.map(function (item, index) {
     console.log(`Fruit ${index}: ${item}`);
@@ -16,9 +29,26 @@ function App() {
     name: 'Bogdan',
     lastName: 'Kichak',
     age: 20,
+    bestFriend: {
+      name: 'Brian',
+      channel: 'BrianChannel',
+    },
   };
 
-  const { name: firstName2, lastName, age, city = 'Kyiv' } = user;
+  const user2 = {
+    ...user,
+  };
+  user2.name = 'Vlad';
+
+  console.log(`User1:`, user);
+  console.log(`User2:`, user2);
+  const {
+    name: firstName2,
+    lastName,
+    age,
+    city = 'Kyiv',
+    bestFriend: { name: friendName },
+  } = user;
 
   const object = {
     name: [variable],
@@ -29,9 +59,13 @@ function App() {
   return (
     <>
       <div>
+        <User {...user} />
+      </div>
+      <div>
         <a href="#">
           Hello, {firstName2}. My city is {city}{' '}
         </a>
+        <p>This is: {friendName}</p>
         {/* <div>Fruit: {object.fruit}</div> */}
       </div>
 
@@ -42,6 +76,10 @@ function App() {
           </div>
         );
       })} */}
+
+      <div>{restFruits[0]}</div>
+
+      <div>first fruit: {restFruits}</div>
     </>
   );
 }
